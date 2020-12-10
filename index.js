b07510e714fb22cd12f4502dbcae6834e14aa77c
Thomas Regulski <tomek.regulski@gmail.com>
2020-12-10 23:37:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string === "I love you") {
+  if (string === "I love you, Grandma") {
     return ("I love you, too.");
   } else if (string === "HELLO") {
     return ("YES INDEED!");
